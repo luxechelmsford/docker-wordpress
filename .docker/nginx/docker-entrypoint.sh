29dc754739fabe7d5aa61e897bc50d0bd892d32b
luxechelmsford <info@theluxestudio.co.uk>
2021-01-10 18:56:29 +0000

    Webdrive cert moved from docker to composer All shell scripts tidied up

--- .docker/nginx/docker-entrypoint.sh
+++ .docker/nginx/docker-entrypoint.sh
@@ -1,66 +1,112 @@
 #!/bin/bash
 set -eu
 
-echo Nginx server staring ...
+echo Nginx service is staring ...
+
+# Checking all variables
+if [ -z "${WORDPRESS_ALL_SERVER_URLS}" ]; then  echo "WORDPRESS_ALL_SERVER_URLS is not set"; fi
+if [ -z "${PHPMYADMIN_ALL_SERVER_URLS}" ]; then  echo "PHPMYADMIN_ALL_SERVER_URLS is not set"; fi
+if [ -z "${LETSENCRYPT_ADMIN_EMAIL_FILE}" ]; then  echo "LETSENCRYPT_ADMIN_EMAIL_FILE is not set"; fi
+
+
 
 # Check and create Nginx Conf, if not created in the previous run
-if [ -f "/etc/nginx/conf.d/wordpress.conf.template" ]
+if [ -n "${WORDPRESS_ALL_SERVER_URLS}" ] && [ -f "/etc/nginx/conf.d/wordpress.conf.template" ]
 then
-  echo "Creating conf file [wordpress.conf] from the template file [[wordpress.conf.template]."
-  if [ -z "${WORDPRESS_ALL_SERVER_URLS}" ]
+  # Do not change the single quotes around ${WORDPRESS_ALL_SERVER_URLS} below to double quotes
+  # Otherwise the environment variables will not be expanded
+  envsubst '${WORDPRESS_ALL_SERVER_URLS}' < /etc/nginx/conf.d/wordpress.conf.template > /etc/nginx/conf.d/wordpress.conf
+
+  if [ -f "/etc/nginx/conf.d/wordpress.conf" ]
   then
-    echo "Failed to create conf file [wordpress.conf] - WORDPRESS_ALL_SERVER_URLS environemnt variable not set";
-    echo "Wordpress all server url list is not set";
-  else   
-    envsubst '${WORDPRESS_ALL_SERVER_URLS}' < /etc/nginx/conf.d/wordpress.conf.template > /etc/nginx/conf.d/wordpress.conf
-
-    if [ ! -f "/etc/nginx/conf.d/wordpress.conf" ]
+    echo "The conf file [wordpress.conf] created successfully ..."
+    echo "Now deleting the template file [wordpress.conf.template] ..."
+    rm -f /etc/nginx/conf.d/wordpress.conf.template
+    if [ ! -f "/etc/nginx/conf.d/wordpress.conf.template" ]
     then
-      echo "Failed to create wordpress.conf."
+      echo "The template files [wordpress.conf.template] deleted successfully ..."
     else
-      echo "The conf file [wordpress.conf] created successfully ..."
-      echo "Now deleteting the template file [wordpress.conf.template] ..."
-      rm -f /etc/nginx/conf.d/wordpress.conf.template
- 
-      if [ -f "/etc/nginx/conf.d/wordpress.conf.template" ]
-      then
-        echo "Failed to delete template files [wordpress.conf.template] ..."
-      else
-        echo "The template files [wordpress.conf.template] deleted successfully ..."
-      fi
+      echo "Failed to delete template files [wordpress.conf.template] ..."
     fi
-  fi
-  
-fi
-
-# Check and create PhpMyAdmin Conf, if not created in the previous run
-if [ -f "/etc/nginx/conf.d/phpmyadmin.conf.template" ]
-then
-  echo "Creating conf file [phpmyadmin.conf] from the template file [[phpmyadmin.conf.template]."
-  if [ -z "${PHPMYADMIN_ALL_SERVER_URLS}" ]
-  then
-    echo "Failed to create conf file [phpmyadmin.conf] - PHPMYADMIN_ALL_SERVER_URLS environemnt variable not set";
-    echo "Wordpress all server url list is not set";
-  else   
-    envsubst '${PHPMYADMIN_ALL_SERVER_URLS}' < /etc/nginx/conf.d/phpmyadmin.conf.template > /etc/nginx/conf.d/phpmyadmin.conf
-
-    if [ ! -f "/etc/nginx/conf.d/phpmyadmin.conf" ]
-    then
-      echo "Failed to create phpmyadmin.conf."
-    else
-      echo "The conf file [phpmyadmin.conf] created successfully ..."
-      echo "Now deleteting the template file [phpmyadmin.conf.template] ..."
-      rm -f /etc/nginx/conf.d/phpmyadmin.conf.template
- 
-      if [ -f "/etc/nginx/conf.d/phpmyadmin.conf.template" ]
-      then
-        echo "Failed to delete template files [phpmyadmin.conf.template] ..."
-      else
-        echo "The template files [phpmyadmin.conf.template] deleted successfully ..."
-      fi
-    fi
+  else
+    echo "Failed to create wordpress.conf."
   fi
 fi
+
+
+
+# Check and create Nginx Conf, if not created in the previous run
+if [ -n "${PHPMYADMIN_ALL_SERVER_URLS}" ] && [ -f "/etc/nginx/conf.d/phpmyadmin.conf.template" ]
+then
+  # Do not change the single quotes around ${PHPMYADMIN_ALL_SERVER_URLS} below to double quotes
+  # Otherwise the environment variables will not be expanded
+  envsubst '${PHPMYADMIN_ALL_SERVER_URLS}' < /etc/nginx/conf.d/phpmyadmin.conf.template > /etc/nginx/conf.d/phpmyadmin.conf
+
+  if [ -f "/etc/nginx/conf.d/phpmyadmin.conf" ]
+  then
+    echo "The conf file [phpmyadmin.conf] created successfully ..."
+    echo "Now deleting the template file [phpmyadmin.conf.template] ..."
+    rm -f /etc/nginx/conf.d/phpmyadmin.conf.template
+    if [ ! -f "/etc/nginx/conf.d/phpmyadmin.conf.template" ]
+    then
+      echo "The template files [phpmyadmin.conf.template] deleted successfully ..."
+    else
+      echo "Failed to delete template files [phpmyadmin.conf.template] ..."
+    fi
+  else
+    echo "Failed to create phpmyadmin.conf."
+  fi
+fi
+
+
+
+# Delete default.conf
+if [ -f /etc/nginx/conf.d/default.conf ]
+then
+  echo "Deleting the default conf files [default.conf] ..."
+  rm -f /etc/nginx/conf.d/default.conf
+  echo "The default conf files [default.conf] deleted successfully ..."
+fi
+
+
+
+# Check if certificate exists
+if [ -n "${WORDPRESS_ALL_SERVER_URLS}" ] && [ -n "${PHPMYADMIN_ALL_SERVER_URLS}" ]
+then
+  if [ ! -f etc/letsencrypt/live/thetek.co.uk/fullchain.pem ] ||  [ ! -f /etc/letsencrypt/live/thetek.co.uk/privkey.pem ]
+  then
+    echo "Installing SSL certificate for ${WORDPRESS_ALL_SERVER_URLS} ${PHPMYADMIN_ALL_SERVER_URLS} ... "
+    if [ -z "${LETSENCRYPT_ADMIN_EMAIL_FILE}" ]
+    then
+      echo "Failed to install letsencrypt certificates - LETSENCRYPT_ADMIN_EMAIL_FILE environemnt variable not set";
+    else
+      ADMIN_EMAIL=$(<"${LETSENCRYPT_ADMIN_EMAIL_FILE}");
+      DOMAIN_LIST=""
+      IFS=', ' read -r -a array <<< "$WORDPRESS_ALL_SERVER_URLS"
+      for element in "${array[@]}"
+      do
+         DOMAIN_LIST="${DOMAIN_LIST} -d ${element}";
+      done      
+      IFS=', ' read -r -a array <<< "$PHPMYADMIN_ALL_SERVER_URLS"
+      for element in "${array[@]}"
+      do
+         DOMAIN_LIST="${DOMAIN_LIST} -d ${element}";
+      done
+echo $DOMAIN_LIST
+      # Do not add any double quoetes on ${DOMAIN_LIST}, as there are multiple options together
+      # if the var is enclsied in double quotes, it will be passed as one single domian - wonn't work 
+      certbot --nginx --non-interactive --agree-tos --email "${ADMIN_EMAIL}" ${DOMAIN_LIST}
+      if [ -f etc/letsencrypt/live/thetek.co.uk/fullchain.pem ] &&  [ -f /etc/letsencrypt/live/thetek.co.uk/privkey.pem ]
+      then
+        echo "Certificate installed successfully ... "
+      fi
+    fi
+  else
+    echo "SSL certificates already exist for wordpress and phpmyadmin"
+  fi
+fi
+
+
 
 # Report if any of the conf is not found
 if [ -f "/etc/nginx/conf.d/wordpress.conf" ]
@@ -77,33 +123,8 @@ else
 fi
 
 
-# Delete default.conf
-if [ -f /etc/nginx/conf.d/default.conf ]
-then
-  echo "Deleting the default conf files [default.conf] ..."
-  rm -f /etc/nginx/conf.d/default.conf
-  echo "The default conf files [default.conf] deleted successfully ..."
-
-  # Check if certificate exists
-  if [ ! -f etc/letsencrypt/live/thetek.co.uk/fullchain.pem ] ||  [ ! -f /etc/letsencrypt/live/thetek.co.uk/privkey.pem]
-  then
-    echo "Installting SSL certificate for ${WORDPRESS_ALL_SERVER_URLS} ${PHPMYADMIN_ALL_SERVER_URLS} ... "
-    if [ -z "${LETSENCRYPT_ADMIN_EMAIL_FILE}" ]
-    then
-      echo "Failed to install letsencrypt certificates - LETSENCRYPT_ADMIN_EMAIL_FILE environemnt variable not set";
-    else
-      ADMIN_EMAIL=$(<$LETSENCRYPT_ADMIN_EMAIL_FILE);  
-      DOMAIN_LIST="-d ${WORDPRESS_ALL_SERVER_URLS/ / -d } -d ${PHPMYADMIN_ALL_SERVER_URLS/ / -d }"
-      certbot --nginx --non-interactive --agree-tos --email ${ADMIN_EMAIL} ${DOMAIN_LIST}
-      if [ -f etc/letsencrypt/live/thetek.co.uk/fullchain.pem ] &&  [ -f /etc/letsencrypt/live/thetek.co.uk/privkey.pem]
-      then
-        echo "Certificate installed successfully ... "
-      fi
-    fi
-  fi
-fi
 
 # Shedule cron task
 /usr/sbin/crond -f -l 8
 
-exec "$@"
+exec "$@"   
--- .docker/webdrive/docker-entrypoint.sh
+++ .docker/webdrive/docker-entrypoint.sh
@@ -1,44 +1,70 @@
 #!/bin/bash
 
-if [ -n "${WEBDRIVE_URL_FILE}" ]; then WEBDRIVE_URL=$(<$WEBDRIVE_URL_FILE); fi
-if [ -z "${WEBDRIVE_URL}" ]; then  echo "Webdrive url is not set"; fi
+echo "Entered webdrive entrypoint script ..."
 
-if [ -n "${WEBDRIVE_UID_FILE}" ]; then WEBDRIVE_UID=$(<$WEBDRIVE_UID_FILE); fi
-if [ -z "${WEBDRIVE_UID}" ]; then  echo "Webdrive uid is not set"; fi
+if [ -z "${WEBDRIVE_CER_FILE}" ]; then  echo "WEBDRIVE_CER_FILE is not set"; fi
 
-if [ -n "${WEBDRIVE_PWD_FILE}" ]; then WEBDRIVE_PWD=$(<$WEBDRIVE_PWD_FILE); fi
-if [ -z "${WEBDRIVE_PWD}" ]; then echo "Webdrive pwd is not set"; fi
+if [ -n "${WEBDRIVE_URL_FILE}" ]; then WEBDRIVE_URL=$(<"${WEBDRIVE_URL_FILE}"); fi
+if [ -z "${WEBDRIVE_URL}" ]; then  echo "WEBDRIVE_URL_FILE is not set"; fi
+
+if [ -n "${WEBDRIVE_UID_FILE}" ]; then WEBDRIVE_UID=$(<"${WEBDRIVE_UID_FILE}"); fi
+if [ -z "${WEBDRIVE_UID}" ]; then  echo "WEBDRIVE_UID_FILE is not set"; fi
+
+if [ -n "${WEBDRIVE_PWD_FILE}" ]; then WEBDRIVE_PWD=$(<"${WEBDRIVE_PWD_FILE}"); fi
+if [ -z "${WEBDRIVE_PWD}" ]; then echo "WEBDRIVE_PWD_FILE is not set"; fi
+
+if [ -z "${WEBDRIVE_REMOTE_PATH}" ]; then echo "WEBDRIVE_REMOTE_PATH is not set"; fi
 
 
-if [ -f /root/.unison/default.prf.template ]
+
+if [ -n "${WEBDRIVE_CER_FILE}" ] &&  [ ! -f "/etc/davfs2/certs/${WEBDRIVE_CER_FILE##*/}" ]
+then
+  echo "Copying Webdrive public certificiate file ..."
+  cp "${WEBDRIVE_CER_FILE}" "/etc/davfs2/certs/"
+  if [ -f "/etc/davfs2/certs/${WEBDRIVE_CER_FILE##*/}" ]
+  then
+    echo "Webdrive public certificiate file copied successfully ..."
+  else
+    echo "Failed to copy Webdrive public certificiate file ..."
+  fi
+fi
+
+
+
+if [ -n "${WEBDRIVE_URL}" ] && [ -n "${WEBDRIVE_UID}" ] &&          \
+   [ -n "${WEBDRIVE_PWD}" ] && [ -n "${WEBDRIVE_REMOTE_PATH}" ] &&  \
+   [ ! -f "/root/.unison/default.prf" ]
 then
 
-  echo "First run detected ..."
-
-  mkdir -p /mnt/webdrive/${WEBDRIVE_REMOTE_PATH}
-  mkdir -p /var/backups/${WEBDRIVE_REMOTE_PATH}
-
+  echo "Setting up webdrive mount configuration ..."
   echo "Creatng default.prf from the template"
   if [ -f "/root/.unison/default.prf.template" ]
   then
+    # Do not change the single quotes around ${WEBDRIVE_REMOTE_PATH} below to double quotes
+    # Otherwise the environment variables will not be expanded
     envsubst '${WEBDRIVE_REMOTE_PATH}' < /root/.unison/default.prf.template > /root/.unison/default.prf
-    echo "The preference file [default.prf] created successfully."
-  elif [ -f "/root/.unison/default.prf" ]
-  then
-    echo "The default.prf has already been created."
+    if  [ -f "/root/.unison/default.prf" ]
+    then
+      echo "The preference file [default.prf] created successfully."
+      echo "Deleating the preference template file [default.prf.template]."
+      rm -f "/root/.unison/default.prf.template"
+
+      if [ ! -f "/root/.unison/default.prf.template" ]
+      then
+        echo "The preference template [default.prf.template] deleated successfully."
+
+        # save webdav drive credentials
+        echo "Saving the webrive credentials in /etc/davfs2/secrets"
+        echo "${WEBDRIVE_URL} ${WEBDRIVE_UID} ${WEBDRIVE_PWD}" >> /etc/davfs2/secrets
+     else
+        echo "Failed to delete the preference template [default.prf.template]."
+      fi
+    else
+      echo "Failed to create preference file [default.prf]."
+    fi
   else
-    echo "Failed to create default.prf. The template file [/root/.unison/default.prf.template] not found."
+    echo "Failed to create preference file default.prf. The template file [/root/.unison/default.prf.template] not found."
   fi
-
-  if [ -f "/root/.unison/default.prf" ]
-  then
-    echo "Deleating the preference template file [default.prf.template]."
-    rm -f /root/.unison/default.prf.template;
-    echo "The preference template [default.prf.template] deleated successfully."
-  fi
-
-  # save webdav drive credentials
-  echo "$WEBDRIVE_URL $WEBDRIVE_UID $WEBDRIVE_PWD" >> /etc/davfs2/secrets
 
   # Create user
   #FOLDER_USER=${SYNC_USERID:-0}
@@ -47,12 +73,38 @@ then
   #fi
 fi
 
-# Mount the webdav drive 
-mount -t davfs $WEBDRIVE_URL /mnt/webdrive -o uid=0,gid=users,dir_mode=755,file_mode=755
-if [ ! -d /mnt/webdrive/${WEBDRIVE_REMOTE_PATH} ]; then
-  mkdir -p /mnt/webdrive/${WEBDRIVE_REMOTE_PATH}
-  echo "Remote directory [/mnt/webdrive/${WEBDRIVE_REMOTE_PATH}] created"
+# Check if the webdrive is mounted
+DRIVE_TYPE=$(stat --file-system --format=%T /mnt/webdrive);
+if [ "${DRIVE_TYPE}" != "fuseblk" ]
+then
+  # Mount the webdav drive
+  echo "Mounting the web drive ..."
+  mount -t davfs "$WEBDRIVE_URL" "/mnt/webdrive" -o uid=0,gid=users,dir_mode=755,file_mode=755
+  DRIVE_TYPE=$(stat --file-system --format=%T /mnt/webdrive);
+  if [ "${DRIVE_TYPE}" == "fuseblk" ]
+  then
+    echo "Webdrive mounted successfully"
+    # only create the remote sub folder once the wedrive is mounted
+    # If it ceated in the local drive folder then it will be mounted as overlayfs
+    # and never as overlayfs and then unison will throw the following error
+    #     Fatal error: Error in canonizing path: No such file or directory
+    if [ ! -d "/mnt/webdrive/${WEBDRIVE_REMOTE_PATH}" ]
+    then
+      mkdir -p "/mnt/webdrive/${WEBDRIVE_REMOTE_PATH}"
+      echo "Remote directory [/mnt/webdrive/${WEBDRIVE_REMOTE_PATH}] created successfully"
+    fi
+    if [ ! -d "/var/backups/${WEBDRIVE_REMOTE_PATH}" ]
+    then
+      mkdir -p "/var/backups/${WEBDRIVE_REMOTE_PATH}"
+      echo "Remote directory [/var/backups/${WEBDRIVE_REMOTE_PATH}] created successfully"
+    fi
+  else
+    echo "Failed to mount the webdrive"
+  fi
+else
+  echo "Webdrive was already mounted"
 fi
 
 # Start the endless sync process
+echo "Starting unison process ..."
 unison
