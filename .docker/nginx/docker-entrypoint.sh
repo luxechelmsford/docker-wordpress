91a8d5d14659f21897ef17717ca497762077538f
luxechelmsford <info@theluxestudio.co.uk>
2021-01-10 03:02:14 +0000

    Lets Encrypt and Redis Server added

--- .docker/nginx/docker-entrypoint.sh
+++ .docker/nginx/docker-entrypoint.sh
@@ -30,6 +30,7 @@ then
       fi
     fi
   fi
+  
 fi
 
 # Check and create PhpMyAdmin Conf, if not created in the previous run
@@ -61,37 +62,6 @@ then
   fi
 fi
 
-
-# Check and create CertBoot Conf, if not created in the previous run
-if [ -f "/etc/nginx/conf.d/certboot.conf.template" ]
-then
-  echo "Creating conf file [certboot.conf] from the template file [[certboot.conf.template]."
-  if [ -z "${CERTBOOT_ALL_SERVER_URLS}" ]
-  then
-    echo "Failed to create conf file [certboot.conf] - CERTBOOT_ALL_SERVER_URLS environemnt variable not set";
-    echo "Wordpress all server url list is not set";
-  else   
-    envsubst '${CERTBOOT_ALL_SERVER_URLS}' < /etc/nginx/conf.d/certboot.conf.template > /etc/nginx/conf.d/certboot.conf
-
-    if [ ! -f "/etc/nginx/conf.d/certboot.conf" ]
-    then
-      echo "Failed to create certboot.conf."
-    else
-      echo "The conf file [certboot.conf] created successfully ..."
-      echo "Now deleteting the template file [certboot.conf.template] ..."
-      rm -f /etc/nginx/conf.d/certboot.conf.template
- 
-      if [ -f "/etc/nginx/conf.d/certboot.conf.template" ]
-      then
-        echo "Failed to delete template files [certboot.conf.template] ..."
-      else
-        echo "The template files [certboot.conf.template] deleted successfully ..."
-      fi
-    fi
-  fi
-fi
-
-
 # Report if any of the conf is not found
 if [ -f "/etc/nginx/conf.d/wordpress.conf" ]
 then
@@ -105,12 +75,6 @@ then
 else
   echo "FATAL ERROR - the phpmyadmin conf file not found - A reinstallation of the container may be required ..."
 fi
-if [ -f "/etc/nginx/conf.d/certboot.conf" ]
-then
-  echo "Certboot conf file found - starting certboot nginx web service ..."
-else
-  echo "FATAL ERROR - the certboot conf file not found - A reinstallation of the container may be required ..."
-fi
 
 
 # Delete default.conf
@@ -119,7 +83,27 @@ then
   echo "Deleting the default conf files [default.conf] ..."
   rm -f /etc/nginx/conf.d/default.conf
   echo "The default conf files [default.conf] deleted successfully ..."
+
+  # Check if certificate exists
+  if [ ! -f etc/letsencrypt/live/thetek.co.uk/fullchain.pem ] ||  [ ! -f /etc/letsencrypt/live/thetek.co.uk/privkey.pem]
+  then
+    echo "Installting SSL certificate for ${WORDPRESS_ALL_SERVER_URLS} ${PHPMYADMIN_ALL_SERVER_URLS} ... "
+    if [ -z "${LETSENCRYPT_ADMIN_EMAIL_FILE}" ]
+    then
+      echo "Failed to install letsencrypt certificates - LETSENCRYPT_ADMIN_EMAIL_FILE environemnt variable not set";
+    else
+      ADMIN_EMAIL=$(<$LETSENCRYPT_ADMIN_EMAIL_FILE);  
+      DOMAIN_LIST="-d ${WORDPRESS_ALL_SERVER_URLS/ / -d } -d ${PHPMYADMIN_ALL_SERVER_URLS/ / -d }"
+      certbot --nginx --non-interactive --agree-tos --email ${ADMIN_EMAIL} ${DOMAIN_LIST}
+      if [ -f etc/letsencrypt/live/thetek.co.uk/fullchain.pem ] &&  [ -f /etc/letsencrypt/live/thetek.co.uk/privkey.pem]
+      then
+        echo "Certificate installed successfully ... "
+      fi
+    fi
+  fi
 fi
 
+# Shedule cron task
+/usr/sbin/crond -f -l 8
 
 exec "$@"
